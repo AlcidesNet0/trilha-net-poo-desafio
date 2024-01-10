771085b4c2e61471019c94f3de17e73465353580
Alcides Neto <alcidesneto40@gmail.com>
2024-01-10 19:42:58 -0300

    Resolução do projeto com novas linhas de código nas classes Nokia, Iphone e Smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("999307171", "2024A", "111111", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("934391440", "Iphone13", "22222", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
